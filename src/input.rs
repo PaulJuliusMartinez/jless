3aff5e90833812376e979859495d55a7e8fe25c8
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2021-04-20 22:37:27 -0700

    Read input from /dev/tty instead of stdin so we can provide JSON data through stdin.

--- src/input.rs
+++ src/input.rs
@@ -2,8 +2,9 @@ use signal_hook::consts::SIGWINCH;
 use signal_hook::low_level::pipe;
 use termion::event::{parse_event, Event, Key, MouseEvent};
 
+use std::fs::File;
 use std::io;
-use std::io::{Read, Stdin};
+use std::io::Read;
 use std::os::unix::io::AsRawFd;
 use std::os::unix::net::UnixStream;
 
@@ -11,9 +12,16 @@ const POLL_INFINITE_TIMEOUT: i32 = -1;
 const SIGWINCH_PIPE_INDEX: usize = 0;
 const BUFFER_SIZE: usize = 1024;
 
-fn read_and_retry_on_interrupt(stdin: &mut Stdin, buf: &mut [u8]) -> io::Result<usize> {
+pub fn get_input() -> impl Iterator<Item = io::Result<TuiEvent>> {
+    let tty = File::open("/dev/tty").unwrap();
+    let (sigwinch_read, sigwinch_write) = UnixStream::pair().unwrap();
+    pipe::register(SIGWINCH, sigwinch_write).unwrap();
+    TuiInput::new(tty, sigwinch_read)
+}
+
+fn read_and_retry_on_interrupt(input: &mut File, buf: &mut [u8]) -> io::Result<usize> {
     loop {
-        match stdin.read(buf) {
+        match input.read(buf) {
             res @ Ok(_) => {
                 return res;
             }
@@ -26,25 +34,18 @@ fn read_and_retry_on_interrupt(stdin: &mut Stdin, buf: &mut [u8]) -> io::Result<
         }
     }
 }
-
-pub fn get_input() -> impl Iterator<Item = io::Result<TuiEvent>> {
-    let (sigwinch_read, sigwinch_write) = UnixStream::pair().unwrap();
-    pipe::register(SIGWINCH, sigwinch_write).unwrap();
-    TuiInput::new(io::stdin(), sigwinch_read)
-}
-
-struct BufferedStdin<const N: usize> {
-    stdin: Stdin,
+struct BufferedInput<const N: usize> {
+    input: File,
     buffer: [u8; N],
     buffer_size: usize,
     buffer_index: usize,
     might_have_more_data: bool,
 }
 
-impl<const N: usize> BufferedStdin<N> {
-    fn new(stdin: Stdin) -> BufferedStdin<N> {
-        BufferedStdin {
-            stdin,
+impl<const N: usize> BufferedInput<N> {
+    fn new(input: File) -> BufferedInput<N> {
+        BufferedInput {
+            input,
             buffer: [0; N],
             buffer_size: 0,
             buffer_index: 0,
@@ -71,7 +72,7 @@ impl<const N: usize> BufferedStdin<N> {
     }
 }
 
-impl<const N: usize> Iterator for BufferedStdin<N> {
+impl<const N: usize> Iterator for BufferedInput<N> {
     type Item = io::Result<u8>;
 
     fn next(&mut self) -> Option<io::Result<u8>> {
@@ -79,12 +80,12 @@ impl<const N: usize> Iterator for BufferedStdin<N> {
             return Some(Ok(self.next_u8()));
         }
 
-        // buffer has been exhausted, clear it and read from stdin again.
+        // buffer has been exhausted, clear it and read from its input again.
         self.buffer_size = 0;
         self.buffer_index = 0;
         self.might_have_more_data = false;
 
-        match read_and_retry_on_interrupt(&mut self.stdin, &mut self.buffer) {
+        match read_and_retry_on_interrupt(&mut self.input, &mut self.buffer) {
             Ok(bytes_read) => {
                 self.buffer_size = bytes_read;
                 self.might_have_more_data = bytes_read == N;
@@ -100,13 +101,13 @@ impl<const N: usize> Iterator for BufferedStdin<N> {
 struct TuiInput {
     poll_fds: [libc::pollfd; 2],
     sigwinch_pipe: UnixStream,
-    buffered_stdin: BufferedStdin<BUFFER_SIZE>,
+    buffered_input: BufferedInput<BUFFER_SIZE>,
 }
 
 impl TuiInput {
-    fn new(stdin: Stdin, sigwinch_pipe: UnixStream) -> TuiInput {
+    fn new(input: File, sigwinch_pipe: UnixStream) -> TuiInput {
         let sigwinch_fd = sigwinch_pipe.as_raw_fd();
-        let stdin_fd = stdin.as_raw_fd();
+        let stdin_fd = input.as_raw_fd();
 
         let poll_fds: [libc::pollfd; 2] = [
             libc::pollfd {
@@ -124,14 +125,14 @@ impl TuiInput {
         TuiInput {
             poll_fds,
             sigwinch_pipe,
-            buffered_stdin: BufferedStdin::new(stdin),
+            buffered_input: BufferedInput::new(input),
         }
     }
 
-    fn get_event_from_buffered_stdin(&mut self) -> Option<io::Result<TuiEvent>> {
-        match self.buffered_stdin.next() {
+    fn get_event_from_buffered_input(&mut self) -> Option<io::Result<TuiEvent>> {
+        match self.buffered_input.next() {
             Some(Ok(byte)) => {
-                return match parse_event(byte, &mut self.buffered_stdin) {
+                return match parse_event(byte, &mut self.buffered_input) {
                     Ok(Event::Key(k)) => Some(Ok(TuiEvent::KeyEvent(k))),
                     Ok(Event::Mouse(m)) => Some(Ok(TuiEvent::MouseEvent(m))),
                     Ok(Event::Unsupported(_)) => Some(Ok(TuiEvent::Unknown)),
@@ -148,8 +149,8 @@ impl Iterator for TuiInput {
     type Item = io::Result<TuiEvent>;
 
     fn next(&mut self) -> Option<io::Result<TuiEvent>> {
-        if self.buffered_stdin.might_have_buffered_data() {
-            return self.get_event_from_buffered_stdin();
+        if self.buffered_input.might_have_buffered_data() {
+            return self.get_event_from_buffered_input();
         }
 
         let poll_res: Option<io::Error>;
@@ -182,7 +183,7 @@ impl Iterator for TuiInput {
             return Some(Ok(TuiEvent::WinChEvent));
         }
 
-        return self.get_event_from_buffered_stdin();
+        return self.get_event_from_buffered_input();
     }
 }
 
