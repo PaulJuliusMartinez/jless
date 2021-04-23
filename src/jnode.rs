c21254c230e86b9964319bda25273fff3650ba9b
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2021-04-22 23:39:51 -0700

    Implement moving up in JSON tree.

--- src/jnode.rs
+++ src/jnode.rs
@@ -24,12 +24,9 @@ impl JNode {
         self.value.is_container()
     }
 
-    fn size(&self) -> usize {
-        debug_assert!(
-            self.is_container(),
-            "cannot call .size on a primitive JNode"
-        );
-        self.value.size()
+    fn len(&self) -> usize {
+        debug_assert!(self.is_container(), "cannot call .len on a primitive JNode");
+        self.value.len()
     }
 
     fn is_empty(&self) -> bool {
@@ -98,17 +95,17 @@ impl JValue {
         !self.is_primitive()
     }
 
-    fn size(&self) -> usize {
+    fn len(&self) -> usize {
         debug_assert!(
             self.is_container(),
-            "cannot call .size on a primitive JValue"
+            "cannot call .len on a primitive JValue"
         );
 
         match self {
             JValue::Array(v) => v.len(),
             JValue::Object(kvp) => kvp.len(),
             JValue::TopLevel(j) => j.len(),
-            _ => panic!("JValue::size called on a primitive"),
+            _ => panic!("JValue::len called on a primitive"),
         }
     }
 
@@ -117,7 +114,7 @@ impl JValue {
             self.is_container(),
             "cannot call .is_empty on a primitive JValue"
         );
-        self.size() == 0
+        self.len() == 0
     }
 }
 
@@ -170,13 +167,13 @@ pub enum Action {
     Down,
     Left,
     Right,
-    ToggleInline,
-    FirstElem,
-    LastElem,
+    // ToggleInline,
+    // FirstElem,
+    // LastElem,
     // NextOccurrenceOfKey
     // PrevOccurrenceOfKey
-    TopOfTree,
-    BottomOfTree,
+    // TopOfTree,
+    // BottomOfTree,
 }
 
 pub fn perform_action(focus: &mut Focus, action: Action) {
@@ -199,14 +196,53 @@ fn validate_focus(focus: &Focus) -> bool {
 
     for (node, index) in focus.0.iter() {
         assert!(node.is_container());
-        assert!(*index < node.size());
+        assert!(*index < node.len());
     }
 
     true
 }
 
-fn move_up(focus: &mut Focus) {}
+// Rules:
+// - If parent is top level node, and you're first child, do nothing.
+// - If you're first child (index == 0), go to parent.
+// - Otherwise, go to previous sibling (index -= 1), then go to its
+//   last child.
+fn move_up(focus: &mut Focus) {
+    // If we're at the very top, do nothing.
+    if focus.0.len() == 1 && focus.0[0].1 == 0 {
+        return;
+    }
 
+    let (parent, ref mut index) = focus.0.last_mut().unwrap();
+
+    if *index == 0 {
+        focus.0.pop();
+        return;
+    }
+
+    *index -= 1;
+    let mut curr_node = &parent[*index];
+
+    while curr_node.is_container() && curr_node.is_expanded() && !curr_node.is_empty() {
+        let last_child_index = curr_node.len() - 1;
+        let next = &curr_node[last_child_index];
+        focus.0.push((curr_node, last_child_index));
+        curr_node = next;
+    }
+}
+
+// Rules:
+// - If current node is primitive, go to next sibling
+// - If current node is collapsed, go to next sibling
+// - If current node is empty, go to next sibling
+// - Otherwise, go to first child
+//
+// - When going to next sibling, if current node is the
+//   last child, go to the next sibling of the parent
+//   (and repeat if parent is also last child)
+//
+// - If actually the last node in the tree, don't modify
+//   focus
 fn move_down(focus: &mut Focus) {
     let (parent_node, index) = focus.0.last().unwrap();
     let current_node = &parent_node[*index];
@@ -215,11 +251,10 @@ fn move_down(focus: &mut Focus) {
     if current_node.is_container() && current_node.is_expanded() && !current_node.is_empty() {
         focus.0.push((current_node, 0));
     } else {
-        // next sibling (easy case is index += 1)
         while depth_index > 0 {
             let (node, curr_index) = focus.0[depth_index];
 
-            if curr_index + 1 < node.size() {
+            if curr_index + 1 < node.len() {
                 focus.0.truncate(depth_index + 1);
                 focus.0[depth_index].1 += 1;
                 break;
@@ -228,15 +263,6 @@ fn move_down(focus: &mut Focus) {
             depth_index -= 1;
         }
     }
-
-    // If current node is expanded, to go first child
-    //      Note: empty array/object can't expand, so act as if collapsed
-    // If current node is collapsed / inlined, go to next sibling
-    // If current node is last child, navigate to next sibling of parent
-    //      (and recurse if necessary)
-    // If actually the last node in the tree, do nothing
-    //
-    // If current node is primitive, go to next sibling
 }
 
 fn move_left(focus: &mut Focus) {}
@@ -248,6 +274,11 @@ mod tests {
 
     const SIMPLE_OBJ: &'static str = r#"{
         "a": { "aa": 1, "ab": 2, "ac": 3 },
+        "b": [1, 2, 3]
+    }"#;
+
+    const SIMPLE_OBJ_WITH_EMPTY: &'static str = r#"{
+        "a": {},
         "b": [1, 2, 3]
     }"#;
 
@@ -291,6 +322,78 @@ mod tests {
         );
     }
 
+    #[test]
+    fn test_movement_down_skips_empty() {
+        let top_level = parse_json(SIMPLE_OBJ_WITH_EMPTY.to_owned()).unwrap();
+        let mut focus: Focus = Focus(vec![(&top_level, 0)]);
+
+        assert_movements(
+            &mut focus,
+            vec![
+                (Action::Down, vec![0, 0].as_slice()),
+                (Action::Down, vec![0, 1].as_slice()),
+                (Action::Down, vec![0, 1, 0].as_slice()),
+            ]
+            .as_slice(),
+        );
+    }
+
+    #[test]
+    fn test_movement_up_simple() {
+        let top_level = parse_json(SIMPLE_OBJ.to_owned()).unwrap();
+        let mut focus = construct_focus(&top_level, &[0, 1, 2]);
+
+        assert_movements(
+            &mut focus,
+            vec![
+                (Action::Up, vec![0, 1, 1].as_slice()),
+                (Action::Up, vec![0, 1, 0].as_slice()),
+                (Action::Up, vec![0, 1].as_slice()),
+                (Action::Up, vec![0, 0, 2].as_slice()),
+                (Action::Up, vec![0, 0, 1].as_slice()),
+                (Action::Up, vec![0, 0, 0].as_slice()),
+                (Action::Up, vec![0, 0].as_slice()),
+                (Action::Up, vec![0].as_slice()),
+                // Stay at top level.
+                (Action::Up, vec![0].as_slice()),
+            ]
+            .as_slice(),
+        );
+    }
+
+    #[test]
+    fn test_movement_up_skips_collapsed() {
+        let mut top_level = parse_json(SIMPLE_OBJ.to_owned()).unwrap();
+        top_level[0][0].state = NodeState::Collapsed;
+        let mut focus: Focus = construct_focus(&top_level, &[0, 1, 0]);
+
+        assert_movements(
+            &mut focus,
+            vec![
+                (Action::Up, vec![0, 1].as_slice()),
+                (Action::Up, vec![0, 0].as_slice()),
+                (Action::Up, vec![0].as_slice()),
+            ]
+            .as_slice(),
+        );
+    }
+
+    #[test]
+    fn test_movement_up_skips_empty() {
+        let top_level = parse_json(SIMPLE_OBJ_WITH_EMPTY.to_owned()).unwrap();
+        let mut focus: Focus = construct_focus(&top_level, &[0, 1, 0]);
+
+        assert_movements(
+            &mut focus,
+            vec![
+                (Action::Up, vec![0, 1].as_slice()),
+                (Action::Up, vec![0, 0].as_slice()),
+                (Action::Up, vec![0].as_slice()),
+            ]
+            .as_slice(),
+        );
+    }
+
     fn assert_focus_indexes(focus: &Focus, indexes: &[usize]) {
         assert_eq!(focus.indexes().as_slice(), indexes);
     }
@@ -306,5 +409,17 @@ mod tests {
             );
             assert_focus_indexes(focus, new_focus_indexes);
         }
+    }
+
+    fn construct_focus<'a, 'b>(top_level: &'a JNode, indexes: &'b [usize]) -> Focus<'a> {
+        let mut curr_node = top_level;
+        let mut focus = Vec::new();
+
+        for index in indexes.iter() {
+            focus.push((curr_node, *index));
+            curr_node = &curr_node[*index];
+        }
+
+        Focus(focus)
     }
 }
