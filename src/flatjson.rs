02c13647754c18b348c31b894e874a7fe48eaf55
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2021-08-06 10:29:49 -0700

    Add simple test for wiring up FlatJson indexes correctly.

--- src/flatjson.rs
+++ src/flatjson.rs
@@ -1,10 +1,31 @@
-use serde_json::value::{Number, Value};
+use serde_json::value::{Number, Value as SerdeValue};
+use std::fmt::Debug;
 
 type Index = usize;
-type OptionIndex = Option<Index>;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum OptionIndex {
+    Nil,
+    Index(Index),
+}
+
+const NIL: usize = usize::MAX;
+
+impl From<usize> for OptionIndex {
+    fn from(i: usize) -> Self {
+        if i == NIL {
+            OptionIndex::Nil
+        } else {
+            OptionIndex::Index(i)
+        }
+    }
+}
 
 #[derive(Debug)]
-pub struct FlatJson {
+pub struct FlatJson(Vec<Row>);
+
+#[derive(Debug)]
+pub struct Row {
     parent: OptionIndex,
     // Should these also be set on the CloseContainers?
     prev_sibling: OptionIndex,
@@ -15,17 +36,17 @@ pub struct FlatJson {
     // start_index: usize
     // end_index: usize
     key: Option<String>,
-    value: FlatJsonValue,
+    value: Value,
 }
 
 #[derive(Debug)]
-enum JsonContainer {
+enum ContainerType {
     Object,
     Array,
 }
 
 #[derive(Debug)]
-enum FlatJsonValue {
+enum Value {
     Null,
     Boolean(bool),
     Number(Number),
@@ -33,76 +54,76 @@ enum FlatJsonValue {
     EmptyObject,
     EmptyArray,
     OpenContainer {
-        container_type: JsonContainer,
+        container_type: ContainerType,
         first_child: Index,
         close_index: Index,
     },
     CloseContainer {
-        container_type: JsonContainer,
+        container_type: ContainerType,
         last_child: Index,
         open_index: Index,
     },
 }
 
-pub fn parse_top_level_json(json: String) -> serde_json::Result<Vec<FlatJson>> {
+pub fn parse_top_level_json(json: String) -> serde_json::Result<FlatJson> {
     let serde_value = serde_json::from_str(&json)?;
     let mut flat_json = vec![];
-    let mut parents = vec![None];
+    let mut parents = vec![OptionIndex::Nil];
 
     flatten_json(serde_value, &mut flat_json, &mut parents);
 
-    Ok(flat_json)
+    Ok(FlatJson(flat_json))
 }
 
-fn flatten_json(serde_value: Value, flat_json: &mut Vec<FlatJson>, parents: &mut Vec<OptionIndex>) {
+fn flatten_json(serde_value: SerdeValue, flat_json: &mut Vec<Row>, parents: &mut Vec<OptionIndex>) {
     let depth = parents.len() - 1;
     let parent = *parents.last().unwrap();
 
-    let value = FlatJson {
+    let row = Row {
         parent,
-        prev_sibling: None,
-        next_sibling: None,
+        prev_sibling: OptionIndex::Nil,
+        next_sibling: OptionIndex::Nil,
         depth,
         index: 0,
         key: None,
-        value: FlatJsonValue::Null,
+        value: Value::Null,
     };
 
     match serde_value {
-        Value::Null => flat_json.push(value),
-        Value::Bool(b) => flat_json.push(FlatJson {
-            value: FlatJsonValue::Boolean(b),
-            ..value
+        SerdeValue::Null => flat_json.push(row),
+        SerdeValue::Bool(b) => flat_json.push(Row {
+            value: Value::Boolean(b),
+            ..row
         }),
-        Value::Number(n) => flat_json.push(FlatJson {
-            value: FlatJsonValue::Number(n),
-            ..value
+        SerdeValue::Number(n) => flat_json.push(Row {
+            value: Value::Number(n),
+            ..row
         }),
-        Value::String(s) => flat_json.push(FlatJson {
-            value: FlatJsonValue::String(s),
-            ..value
+        SerdeValue::String(s) => flat_json.push(Row {
+            value: Value::String(s),
+            ..row
         }),
-        Value::Array(vs) => {
+        SerdeValue::Array(vs) => {
             if vs.len() == 0 {
-                flat_json.push(FlatJson {
-                    value: FlatJsonValue::EmptyArray,
-                    ..value
+                flat_json.push(Row {
+                    value: Value::EmptyArray,
+                    ..row
                 })
             } else {
                 let open_index = flat_json.len();
-                parents.push(Some(open_index));
+                parents.push(OptionIndex::Index(open_index));
 
-                flat_json.push(FlatJson {
-                    value: FlatJsonValue::OpenContainer {
-                        container_type: JsonContainer::Array,
+                flat_json.push(Row {
+                    value: Value::OpenContainer {
+                        container_type: ContainerType::Array,
                         first_child: open_index + 1,
                         // Set once done processing the array.
                         close_index: 0,
                     },
-                    ..value
+                    ..row
                 });
 
-                let mut prev_sibling: OptionIndex = None;
+                let mut prev_sibling: OptionIndex = OptionIndex::Nil;
                 let mut child_index = 0;
 
                 for (i, v) in vs.into_iter().enumerate() {
@@ -114,31 +135,32 @@ fn flatten_json(serde_value: Value, flat_json: &mut Vec<FlatJson>, parents: &mut
                     child.index = i;
                     child.prev_sibling = prev_sibling;
 
-                    if let Some(prev_sibling_index) = prev_sibling {
-                        flat_json[prev_sibling_index].next_sibling = Some(child_index);
+                    if let OptionIndex::Index(prev_sibling_index) = prev_sibling {
+                        flat_json[prev_sibling_index].next_sibling =
+                            OptionIndex::Index(child_index);
                     }
 
-                    prev_sibling = Some(child_index);
+                    prev_sibling = OptionIndex::Index(child_index);
                 }
 
                 let close_index = flat_json.len();
-                flat_json.push(FlatJson {
+                flat_json.push(Row {
                     parent,
                     // Currently not set on the CloseContainer value.
-                    prev_sibling: None,
-                    next_sibling: None,
+                    prev_sibling: OptionIndex::Nil,
+                    next_sibling: OptionIndex::Nil,
                     depth,
                     index: 0,
                     key: None,
-                    value: FlatJsonValue::CloseContainer {
-                        container_type: JsonContainer::Array,
+                    value: Value::CloseContainer {
+                        container_type: ContainerType::Array,
                         last_child: child_index,
                         // Set once done processing the array.
                         open_index,
                     },
                 });
 
-                if let FlatJsonValue::OpenContainer {
+                if let Value::OpenContainer {
                     close_index: ref mut close_index_of_open_value,
                     ..
                 } = &mut flat_json[open_index].value
@@ -149,27 +171,27 @@ fn flatten_json(serde_value: Value, flat_json: &mut Vec<FlatJson>, parents: &mut
                 parents.pop();
             }
         }
-        Value::Object(obj) => {
+        SerdeValue::Object(obj) => {
             if obj.len() == 0 {
-                flat_json.push(FlatJson {
-                    value: FlatJsonValue::EmptyObject,
-                    ..value
+                flat_json.push(Row {
+                    value: Value::EmptyObject,
+                    ..row
                 })
             } else {
                 let open_index = flat_json.len();
-                parents.push(Some(open_index));
+                parents.push(OptionIndex::Index(open_index));
 
-                flat_json.push(FlatJson {
-                    value: FlatJsonValue::OpenContainer {
-                        container_type: JsonContainer::Object,
+                flat_json.push(Row {
+                    value: Value::OpenContainer {
+                        container_type: ContainerType::Object,
                         first_child: open_index + 1,
                         // Set once done processing the array.
                         close_index: 0,
                     },
-                    ..value
+                    ..row
                 });
 
-                let mut prev_sibling: OptionIndex = None;
+                let mut prev_sibling: OptionIndex = OptionIndex::Nil;
                 let mut child_index = 0;
 
                 for (i, (k, v)) in obj.into_iter().enumerate() {
@@ -182,31 +204,32 @@ fn flatten_json(serde_value: Value, flat_json: &mut Vec<FlatJson>, parents: &mut
                     child.prev_sibling = prev_sibling;
                     child.key = Some(k);
 
-                    if let Some(prev_sibling_index) = prev_sibling {
-                        flat_json[prev_sibling_index].next_sibling = Some(child_index);
+                    if let OptionIndex::Index(prev_sibling_index) = prev_sibling {
+                        flat_json[prev_sibling_index].next_sibling =
+                            OptionIndex::Index(child_index);
                     }
 
-                    prev_sibling = Some(child_index);
+                    prev_sibling = OptionIndex::Index(child_index);
                 }
 
                 let close_index = flat_json.len();
-                flat_json.push(FlatJson {
+                flat_json.push(Row {
                     parent,
                     // Currently not set on the CloseContainer value.
-                    prev_sibling: None,
-                    next_sibling: None,
+                    prev_sibling: OptionIndex::Nil,
+                    next_sibling: OptionIndex::Nil,
                     depth,
                     index: 0,
                     key: None,
-                    value: FlatJsonValue::CloseContainer {
-                        container_type: JsonContainer::Object,
+                    value: Value::CloseContainer {
+                        container_type: ContainerType::Object,
                         last_child: child_index,
                         // Set once done processing the array.
                         open_index,
                     },
                 });
 
-                if let FlatJsonValue::OpenContainer {
+                if let Value::OpenContainer {
                     close_index: ref mut close_index_of_open_value,
                     ..
                 } = &mut flat_json[open_index].value
@@ -216,6 +239,56 @@ fn flatten_json(serde_value: Value, flat_json: &mut Vec<FlatJson>, parents: &mut
 
                 parents.pop();
             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const OBJECT_1: &'static str = r#"{
+        "a": 2,
+        "b": [
+            4,
+            "5"
+        ],
+        "c": {
+            "d": null,
+            "e": true,
+            "f": 9
+        },
+        "g": 11
+    }"#;
+
+    #[test]
+    fn test_flatten_json() {
+        let fj = parse_top_level_json(OBJECT_1.to_owned()).unwrap();
+
+        assert_prev_siblings(
+            &fj,
+            vec![NIL, NIL, 1, NIL, 3, NIL, 2, NIL, 7, 8, NIL, 6, NIL],
+        )
+    }
+
+    fn assert_prev_siblings<T: Into<OptionIndex> + Debug + Copy>(
+        fj: &FlatJson,
+        prev_siblings: Vec<T>,
+    ) {
+        assert_eq!(
+            fj.0.len(),
+            prev_siblings.len(),
+            "length of prev_siblings is incorrect"
+        );
+
+        for (i, (elem, expected_prev_sibling)) in fj.0.iter().zip(prev_siblings.iter()).enumerate()
+        {
+            assert_eq!(
+                Into::<OptionIndex>::into(*expected_prev_sibling),
+                elem.prev_sibling,
+                "incorrect prev_sibling at index {}",
+                i
+            );
         }
     }
 }
